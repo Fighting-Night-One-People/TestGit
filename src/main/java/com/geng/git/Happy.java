0812a60212c014f627828d031b048e960d684308
GENGSS <gengshuaishuai@163.com>
2020-05-14 14:53:42 +0800

    commit hot_fix

--- src/main/java/com/geng/git/Happy.java
+++ src/main/java/com/geng/git/Happy.java
@@ -3,5 +3,6 @@ package com.geng.git;
 public class Happy {
 	public static void main(String[] args){
 		System.out.println("console message left ...");
+		System.out.println("console hot_fix");
 	}
 }
